55667a500dd630e5f5c0f7968c6c064558fac192
Anton Efimenko <reloni@ya.ru>
2016-07-10 16:06:49 +0300

    Exclude git submodules and add workspace that include local RxHttpClient project

--- Scripts/CarthageUpdate.sh
+++ Scripts/CarthageUpdate.sh
@@ -1,9 +1,9 @@
 #rm -rf ~/Library/Caches/org.carthage.CarthageKit
-carthage update --no-build --use-submodules
+carthage update --no-build
 carthage build RxSwift --platform iOS
 carthage build JASON --platform iOS
 carthage build RxHttpClient --platform iOS
-carthage build AliSoftware/OHHTTPStubs --platform iOS
+carthage build OHHTTPStubs --platform iOS
 mkdir -p ./Dependencies/iOS
 ./Scripts/CopyFrameworks.sh
 #cp -R ./Carthage/Build/iOS/*.framework ./Dependencies/iOS
